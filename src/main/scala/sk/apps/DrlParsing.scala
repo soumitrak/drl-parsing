d1d6babb012b7347db1214b191b251ad1025338c
soumitrak <soumitrak@users.noreply.github.com>
2015-11-14 06:46:14 -0800

    Create DrlParsing.scala

---
+++ src/main/scala/sk/apps/DrlParsing.scala
@@ -0,0 +1,22 @@
+package sk.apps
+
+import java.io.File
+
+import com.typesafe.scalalogging.LazyLogging
+
+import org.kie.api.io.ResourceType
+import org.kie.internal.io.ResourceFactory
+import org.kie.internal.builder.KnowledgeBuilderFactory
+
+object DrlParsing extends LazyLogging {
+  def main (args: Array [String]): Unit = {
+    logger.info (s"Starting KnowledgeBuilder")
+    val kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder ()
+    args.foreach { fileName =>
+      logger.info (s"Adding file $fileName")
+      kbuilder.add (ResourceFactory.newFileResource (new File (fileName)), ResourceType.DRL)
+    }
+    if (kbuilder.hasErrors ())
+      logger.error (kbuilder.getErrors ().toString ())
+  }
+}
